1149f0c799df3875249aaf4bcf200f8925e36c55
Philip Abbey <philipabbey@users.noreply.github.com>
2022-11-06 14:40:24 +0000

    Batch files for installation
    Install customasm, and amended VS Code extension installation.

--- install_extension.bat
+++ install_extension.bat
@@ -1,5 +1,22 @@
 @echo off
+rem ---------------------------------------------------------------------------------
+rem 
+rem  Distributed under MIT Licence
+rem    See https://github.com/house-of-abbey/scratch_vhdl/blob/main/LICENCE.
+rem 
+rem  J D Abbey & P A Abbey, 23 October 2022
+rem 
+rem ---------------------------------------------------------------------------------
 
 title Installing Extensions
-curl https://github.com/house-of-abbey/scratch_vhdl/releases/latest/download/scratch-vhdl-vscode.vsix --output ./scratch-vhdl-vscode.vsix
-code --install-extension ./scratch-vhdl-vscode.vsix
+curl ^
+  --silent ^
+  --location ^
+  --output %TEMP%/scratch-vhdl-vscode.vsix ^
+  https://github.com/house-of-abbey/scratch_vhdl/releases/latest/download/scratch-vhdl-vscode.vsix
+
+code --install-extension %TEMP%/scratch-vhdl-vscode.vsix
+code --install-extension josephabbey.customasm
+code --install-extension puorc.awesome-vhdl
+
+del /f %TEMP%/scratch-vhdl-vscode.vsix
