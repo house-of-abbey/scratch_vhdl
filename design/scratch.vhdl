316d06aa961aaa28c59078ff2afd3dfc2543926b
Philip Abbey <philipabbey@users.noreply.github.com>
2022-11-11 19:02:52 +0000

    Update scratch.vhdl
    Added headers and therefore description.

--- design/scratch.vhdl
+++ design/scratch.vhdl
@@ -1,3 +1,17 @@
+-------------------------------------------------------------------------------------
+--
+-- Distributed under MIT Licence
+--   See https://github.com/house-of-abbey/scratch_vhdl/blob/main/LICENCE.
+--
+-------------------------------------------------------------------------------------
+--
+-- This file is overwritten by Scratch VHDL code generation. We just need a stub in
+-- order to add the file to a Vivado project.
+--
+-- J D Abbey & P A Abbey, 27 October 2022
+--
+-------------------------------------------------------------------------------------
+
 architecture scratch of led4_button4 is
 begin
 end architecture;
