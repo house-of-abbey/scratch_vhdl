35aa40de0f56b9dd695b2e2b13d7736a8dff2033
Joseph Abbey <joseph@abbey1.org.uk>
2022-11-12 14:07:15 +0000

    risc_cpu ins scratch

--- examples/risc_cpu.vhdl
+++ examples/risc_cpu.vhdl
@@ -10,59 +10,124 @@ architecture scratch of led4_button4 is
   constant button_tab_c : natural := 2;
   constant code : work.risc_cpu_pkg.t_code := (x"1030",x"3100",x"7201",x"4200",x"e001");
 
-  signal cnt : signed(11 downto 0);
-  signal mem : work.risc_cpu_pkg.t_mem;
+  signal pc : std_logic_vector(8 downto 0);
+  signal pc_value : work.risc_cpu_pkg.code_t'element;
+  signal pc_op : work.risc_cpu_pkg.op_t;
+  signal pc_dest : natural range 0 to 7;
+  signal pc_src1 : natural range 0 to 7;
+  signal pc_src2 : natural range 0 to 7;
+  signal wi : unsigned(8 downto 0);
+  signal eif : std_logic;
+  signal reg : work.risc_cpu_pkg.reg_arr_t;
 
 
 begin
+
+  leds <= reg(7);
+
+  pc_value <= code(to_integer(unsigned(pc)));
+
+  pc_op <= pc_value(12 downto 9);
+
+  pc_dest <= to_integer(unsigned(pc_value(8 downto 6)));
+
+  pc_src1 <= to_integer(unsigned(pc_value(5 downto 3)));
+
+  pc_src2 <= to_integer(unsigned(pc_value(2 downto 0)));
 
   process(clk)
   begin
     if rising_edge(clk) then
       if reset = '1' then
-        leds <= "0000";
-        cnt <= (others => '0');
-        mem <= (others => (others => '0'));
+        pc <= (others => '0');
+        reg <= (others => (others => '0'));
+        wi <= (others => '0');
+        eif <= '0';
       else
-        cnt <= cnt + 1;
-        case code(to_integer(cnt))(15 downto 12) is
-          when "0001" =>
-            mem(to_integer(unsigned(code(to_integer(cnt))(11 downto 8)))) <= code(to_integer(cnt))(7 downto 4);
+        -- The assembler will prevent assignments to reg(6). If the assembled
+        -- instruction code does manage to assign reg(6), it will be overwritten
+        -- on the next clock cycle.
+        reg(6) <= buttons;
+        if incr = '1' then
+          if wi > 0 then
+            wi <= wi - 1;
+          end if;
+        end if;
+        -- wi is all zeros
+        if nor(wi) then
+          if eif = '1' then
+            pc <= pc + 2;
+            eif <= '0';
+          else
+            pc <= pc + 1;
+          end if;
+          case pc_op is
+            when "0001" =>
+              reg(pc_dest) <= pc_value(3 to 0);
 
-          when "0010" =>
-            mem(to_integer(unsigned(code(to_integer(cnt))(11 downto 8)))) <= mem(to_integer(unsigned(code(to_integer(cnt))(7 downto 4))));
+            when "0010" =>
+              reg(pc_dest) <= reg(pc_src1);
 
-          when "0011" =>
-            mem(to_integer(unsigned(code(to_integer(cnt))(11 downto 8)))) <= buttons;
+            when "0011" =>
+              reg(pc_dest) <= reg(pc_src1) and reg(pc_src1);
 
-          when "0100" =>
-            leds <= mem(to_integer(unsigned(code(to_integer(cnt))(11 downto 8))));
+            when "0100" =>
+              reg(pc_dest) <= reg(pc_src1) or reg(pc_src1);
 
-          when "0101" =>
-            mem(to_integer(unsigned(code(to_integer(cnt))(11 downto 8)))) <= mem(to_integer(unsigned(code(to_integer(cnt))(7 downto 4)))) and mem(to_integer(unsigned(code(to_integer(cnt))(3 downto 0))));
+            when "0101" =>
+              reg(pc_dest) <= not reg(pc_src1);
 
-          when "0110" =>
-            mem(to_integer(unsigned(code(to_integer(cnt))(11 downto 8)))) <= mem(to_integer(unsigned(code(to_integer(cnt))(7 downto 4)))) or mem(to_integer(unsigned(code(to_integer(cnt))(3 downto 0))));
+            when "0110" =>
+              reg(pc_dest) <= reg(pc_src1) + reg(pc_src1);
 
-          when "0111" =>
-            mem(to_integer(unsigned(code(to_integer(cnt))(11 downto 8)))) <= mem(to_integer(unsigned(code(to_integer(cnt))(7 downto 4))));
+            when "0111" =>
+              reg(pc_dest) <= reg(pc_src1) - reg(pc_src1);
 
-          when "1000" =>
-            mem(to_integer(unsigned(code(to_integer(cnt))(11 downto 8)))) <= mem(to_integer(unsigned(code(to_integer(cnt))(7 downto 4))));
+            when "1000" =>
+              if pc_value(1) = '0' then
+                reg(pc_dest) <= pc_value(0) & reg(pc_src1)(3 downto 1);
+              else
+                reg(pc_dest) <= reg(pc_src1)(2 downto 0) & pc_value(0);
+              end if;
 
-          when "1110" =>
-            cnt <= signed(code(to_integer(cnt)))(11 downto 0);
+            when "1011" =>
+              if reg(pc_src1) = reg(pc_src2) then
+                eif <= '1';
+              else
+                pc <= pc + 2;
+              end if;
 
-          when "1111" =>
-            cnt <= cnt + signed(code(to_integer(cnt)))(11 downto 0);
+            when "1100" =>
+              if reg(pc_src1) > reg(pc_src2) then
+                eif <= '1';
+              else
+                pc <= pc + 2;
+              end if;
 
-          when "0000" =>
+            when "1101" =>
+              if reg(pc_src1) >= reg(pc_src2) then
+                eif <= '1';
+              else
+                pc <= pc + 2;
+              end if;
 
-          when others =>
+            when "1110" =>
+              wi <= unsigned(pc_value(8 downto 0));
 
-        end case;
+            when "1111" =>
+              pc <= pc_value(8 downto 0);
+              eif <= '0';
+
+            when others =>
+
+          end case;
+        end if;
       end if;
     end if;
   end process;
+pc <= pc + 1;
+
+pc <= pc + 1;
+
 
 end architecture;
--- examples/risc_cpu_pkg.vhdl
+++ examples/risc_cpu_pkg.vhdl
@@ -1,9 +1,91 @@
 library ieee;
   use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
 
 package risc_cpu_pkg is
 
-  type t_code is array (0 to 4) of std_logic_vector(15 downto 0);
-  type t_mem is array (0 to 3) of std_logic_vector(3 downto 0);
+  type reg_arr_t is array (0 to 7) of std_logic_vector(3 downto 0);
+
+  function "+" (l, r : std_logic_vector)           return std_logic_vector;
+  function "+" (l : std_logic_vector; r : signed)  return std_logic_vector;
+  function "+" (l : std_logic_vector; r : integer) return std_logic_vector;
+  function "-" (l, r : std_logic_vector)           return std_logic_vector;
+
+  type code_t is array (natural range <>) of std_logic_vector(12 downto 0);
+
+  impure function read_code_from_file(
+    filename : string;
+    arr_size : natural
+  ) return code_t;
 
 end package;
+
+
+library std;
+  use std.textio.all;
+
+package body risc_cpu_pkg is
+
+  function "+" (l, r : std_logic_vector) return std_logic_vector is
+  begin
+    return std_logic_vector(signed(l) + signed(r));
+  end function;
+
+  function "+" (l : std_logic_vector; r : signed) return std_logic_vector is
+  begin
+    return std_logic_vector(signed(l) + r);
+  end function;
+
+  function "+" (l : std_logic_vector; r : integer) return std_logic_vector is
+  begin
+    return std_logic_vector(signed(l) + r);
+  end function;
+
+  function "-" (l, r : std_logic_vector) return std_logic_vector is
+  begin
+    return std_logic_vector(signed(l) - signed(r));
+  end function;
+  
+  function str2bin(s: string) return std_logic_vector is
+    variable ret : std_logic_vector(s'range);
+    
+    function s2b(c : character) return std_logic is
+    begin
+      if c = '1' then
+        return '1';
+      else
+        -- Any other character at all
+        return '0';
+      end if;
+    end function;
+
+  begin
+    for i in s'range loop
+      ret(i) := s2b(s(i));
+    end loop;
+    return ret;
+  end function;
+
+  impure function read_code_from_file(
+    filename : string;
+    arr_size : natural
+  ) return code_t is
+    file fh      : text open read_mode is filename;
+    variable l   : line;
+    variable s   : string(1 to 13);         -- Each line should be 13 characters long.
+    variable ret : code_t(0 to arr_size-1); -- Get this passed down derived from the number of address bits driving the ROM.
+    variable ln  : natural range 0 to arr_size-1 := 0;
+  begin
+    while not endfile(fh) loop 
+      readline(fh, l);
+      -- Cannot provide any file reading error checking, e.g. for lines less than 13
+      -- characters, because "l.all" is not supported for synthesis, even when
+      -- initialising a constant.
+      read(l, s);
+      ret(ln) := str2bin(s); -- Convert binary string to vector. Requires a function.
+      ln := ln + 1;
+    end loop;
+    return ret;
+  end function;
+
+end package body;
