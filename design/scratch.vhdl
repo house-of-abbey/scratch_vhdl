cb8bec1fa0b9ef5ce2f47bfc5fff742a992d38ec
Philip Abbey <philip@abbey1.org.uk>
2023-05-20 12:19:47 +0100

    Create scratch.vhdl
    Needs to be present, but will be overwritten, so don't track changes,

---
+++ design/scratch.vhdl
@@ -0,0 +1,21 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+
+architecture scratch of led4_button4 is
+
+  constant button_tab_c : natural := 2;
+
+begin
+
+  process(clk)
+  begin
+    if rising_edge(clk) then
+      if reset = '1' then
+        leds <= "0000";
+      else
+        leds <= buttons;
+      end if;
+    end if;
+  end process;
+
+end architecture;
