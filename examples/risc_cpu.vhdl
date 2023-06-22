26e11f379c7a549f0e98446d4672cf338423f7aa
Philip Abbey <philipabbey@users.noreply.github.com>
2023-06-22 20:44:37 +0100

    Added Scratch comments to op codes in the RISC CPU design

--- examples/risc_cpu.vhdl
+++ examples/risc_cpu.vhdl
@@ -60,6 +60,7 @@ begin
         end if;
         -- wi is all zeros
         if nor(wi) then
+          -- Update the program counter
           if eif = '1' then
             pc <= pc + 2;
             eif <= '0';
@@ -67,34 +68,45 @@ begin
             pc <= pc + 1;
           end if;
           case pc_op is
+            -- op_set
             when "0001" =>
               reg(pc_dest) <= pc_value(3 downto 0);
 
+            -- op_copy
             when "0010" =>
               reg(pc_dest) <= reg(pc_src1);
 
+            -- op_and
             when "0011" =>
               reg(pc_dest) <= reg(pc_src1) and reg(pc_src1);
 
+            -- op_or
             when "0100" =>
               reg(pc_dest) <= reg(pc_src1) or reg(pc_src1);
 
+            -- op_not
             when "0101" =>
               reg(pc_dest) <= not reg(pc_src1);
 
+            -- op_add
             when "0110" =>
               reg(pc_dest) <= reg(pc_src1) + reg(pc_src2);
 
+            -- op_sub
             when "0111" =>
               reg(pc_dest) <= reg(pc_src1) - reg(pc_src2);
 
+            -- op_shft
             when "1000" =>
               if pc_value(1) = '0' then
+                -- Shift right
                 reg(pc_dest) <= pc_value(0) & reg(pc_src1)(3 downto 1);
               else
+                -- Shit left
                 reg(pc_dest) <= reg(pc_src1)(2 downto 0) & pc_value(0);
               end if;
 
+            -- op_ifbit
             when "1010" =>
               if reg(pc_src1)(to_integer(unsigned(pc_value(1 downto 0)))) = '1' then
                 eif <= '1';
@@ -102,6 +114,7 @@ begin
                 pc <= pc + 2;
               end if;
 
+            -- op_ifeq
             when "1011" =>
               if reg(pc_src1) = reg(pc_src2) then
                 eif <= '1';
@@ -109,6 +122,7 @@ begin
                 pc <= pc + 2;
               end if;
 
+            -- op_ifgt
             when "1100" =>
               if reg(pc_src1) > reg(pc_src2) then
                 eif <= '1';
@@ -116,6 +130,7 @@ begin
                 pc <= pc + 2;
               end if;
 
+            -- op_ifge
             when "1101" =>
               if reg(pc_src1) >= reg(pc_src2) then
                 eif <= '1';
@@ -123,9 +138,11 @@ begin
                 pc <= pc + 2;
               end if;
 
+            -- op_wi
             when "1110" =>
               wi <= unsigned(pc_value(8 downto 0));
 
+            -- op_goto
             when "1111" =>
               pc <= pc_value(8 downto 0);
               eif <= '0';
