f71ceb7eaa9d2fd76931f3802001d9c53e9b4ac3
Joseph Abbey <me@josephabbey.dev>
2023-06-24 11:55:03 +0100

    asm_compile button

--- scratch-vhdl-vscode/src/extension.ts
+++ scratch-vhdl-vscode/src/extension.ts
@@ -1,5 +1,8 @@
+/* eslint-disable no-control-regex */
 import * as vscode from 'vscode';
 import { ScratchVHDLEditorProvider } from './scratchVHDLEditor';
+import cp = require('child_process');
+import path = require('path');
 
 export function activate(context: vscode.ExtensionContext) {
   // add custom file nesting rules
@@ -14,4 +17,138 @@ export function activate(context: vscode.ExtensionContext) {
   );
   // Register our custom editor providers
   context.subscriptions.push(ScratchVHDLEditorProvider.register(context));
+
+  const assemble = (document: vscode.TextDocument) => {
+    document.save();
+    let output = cp.execSync(
+      `${path.join(
+        vscode.workspace.workspaceFile?.fsPath ?? '',
+        '..',
+        configuration
+          .get<string>('scratch-vhdl-vscode.asm_compile_path')
+          ?.replace(/\//g, '\\') ?? ''
+      )} ${document.uri.fsPath}`,
+      {
+        encoding: 'utf-8',
+      }
+    );
+    output = output.replace(
+      / --> asm:\x1b\[0m\x1b\[90m(\d+):(\d+)/g,
+      (_, line, column) =>
+        ` --> asm:<button class="a" onclick="window.goto(${line - 1},${
+          column - 1
+        })">${line}:${column}</button>`
+    );
+
+    output = output.replace(
+      / --> asm:\x1b\[0m\x1b\[90m(\d+):(\d+)/g,
+      (_, line, column) =>
+        ` --> asm:<button class="a" onclick="window.goto(${line - 1},${
+          column - 1
+        })">${line}:${column}</button>`
+    );
+    output = output.replace(
+      /\x1b\[90m/g,
+      "</span><span style='color:var(--vscode-disabledForeground);'>"
+    );
+    output = output.replace(
+      /\x1b\[91m/g,
+      "</span><span style='color:var(--vscode-errorForeground);'>"
+    );
+    output = output.replace(/\x1b\[93m/g, "</span><span style='color:#f80;'>");
+    output = output.replace(/\x1b\[96m/g, "</span><span style='color:#08f;'>");
+    output = output.replace(
+      /\x1b\[97m/g,
+      "</span><span style='color:var(--vscode-foreground);'>"
+    );
+    output = output.replace(
+      /\x1b\[1m/g,
+      "</span><span style='font-weight:bold;'>"
+    );
+    output = output.replace(
+      /\x1b\[0m/g,
+      "</span><span style='color:var(--vscode-foreground);'>"
+    );
+
+    output =
+      "<span style='color:var(--vscode-foreground);'>" + output + '</span>';
+
+    return output;
+  };
+
+  context.subscriptions.push(
+    vscode.commands.registerTextEditorCommand(
+      'scratch-vhdl-vscode.asm_compile',
+      (editor) => {
+        const panel = vscode.window.createWebviewPanel(
+          'customasm',
+          `Customasm`,
+          vscode.ViewColumn.Beside,
+          {
+            enableScripts: true,
+          }
+        );
+
+        panel.webview.onDidReceiveMessage(
+          (message) => {
+            switch (message.command) {
+              case 'goto': {
+                const a = message.text.split(' ');
+                const l = parseInt(a[0]);
+                const c = parseInt(a[1]);
+                editor.revealRange(
+                  new vscode.Range(
+                    new vscode.Position(l, c),
+                    new vscode.Position(l, c)
+                  )
+                );
+                editor.selection = new vscode.Selection(l, 0, l, 999);
+                break;
+              }
+            }
+          },
+          undefined,
+          context.subscriptions
+        );
+
+        panel.webview.html = `<!DOCTYPE html>
+                            <html lang="en">
+                                <head>
+                                    <title>customasm</title>
+                                    <meta charset="utf-8">
+                                    <style>
+                                        #output {
+                                            width: 100px;
+                                            height: 100px;
+                                            font-family: Consolas, monospace;
+                                            white-space: pre;
+                                        }
+
+                                        button.a {
+                                            background: none!important;
+                                            border: none;
+                                            padding: 0!important;
+                                            font-family: inherit;
+                                            color: var(--vscode-textLink-foreground);
+                                            cursor: pointer;
+                                        }
+
+                                        button.a:hover {
+                                            text-decoration: underline;
+                                        }
+                                    </style>
+                                </head>
+                                <body>
+                                    <script>
+                                        window.vscode = acquireVsCodeApi();
+                                        window.goto = (l, c) => window.vscode.postMessage({ command: "goto", text: l+" "+c });
+                                    </script>
+                                    <div id="output">${assemble(
+                                      editor.document
+                                    )}</div>
+                                </body>
+                            </html>`;
+      }
+    )
+  );
 }
