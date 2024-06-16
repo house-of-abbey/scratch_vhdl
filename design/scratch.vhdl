9f563bcdabda5fc183f1fbcb571daf4961a32511
Philip Abbey <philipabbey@users.noreply.github.com>
2024-06-16 20:46:00 +0100

    Reverse 3 files that should not have been committed

--- design/scratch.vhdl
+++ design/scratch.vhdl
@@ -8,28 +8,17 @@ architecture scratch of led4_button4 is
 
   signal start_r : std_logic := '0';
   signal stop_r : std_logic := '0';
-  signal reg : std_logic_vector(5 downto 0) := "000000";
+  signal state : natural range 0 to 7 := 0;
 
-  alias start is buttons(0);
-  alias stop is buttons(1);
 
 begin
--- Solution contributed by A. Sutton.
 
   process(clk)
   begin
     if rising_edge(clk) then
       if reset = '1' then
-        leds <= "1000";
-        reg <= "100000";
+        leds <= "0000";
       else
-        if incr = '1' then
-          reg <= reg(0) & reg(5 downto 1);
-        end if;
-        leds(3) <= reg(5);
-        leds(2) <= reg(4) or reg(0);
-        leds(1) <= reg(3) or reg(1);
-        leds(0) <= reg(2);
       end if;
     end if;
   end process;
