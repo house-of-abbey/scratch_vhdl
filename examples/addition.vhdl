3856e8983616dd645000d0e226248fa2378550dc
Philip Abbey <philipabbey@users.noreply.github.com>
2026-03-21 15:34:38 +0000

    Simplified binary addition

--- examples/addition.vhdl
+++ examples/addition.vhdl
@@ -6,10 +6,6 @@ architecture scratch of led4_button4 is
 
   constant button_tab_c : natural := 2;
 
-  signal start_r : std_logic := '0';
-  signal stop_r : std_logic := '0';
-  signal state : natural range 0 to 7 := 0;
-  signal buttons_d : std_logic_vector(3 downto 0);
 
   alias start is buttons(0);
   alias stop is buttons(1);
@@ -24,7 +20,8 @@ begin
       else
         leds(0) <= buttons(0) xor buttons(2);
         leds(1) <= (buttons(0) and buttons(2)) xor (buttons(1) xor buttons(3));
-        leds(2) <= ((buttons(0) and buttons(2)) and buttons(1)) or (((buttons(0) and buttons(2)) and buttons(3)) or (buttons(3) and buttons(1)));
+        -- One of the XOR and OR logic gates can be interchanged.
+        leds(2) <= (buttons(3) and buttons(1)) or ((buttons(3) or buttons(1)) and (buttons(2) and buttons(0)));
       end if;
     end if;
   end process;
